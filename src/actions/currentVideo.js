88c3a345b0b3ebf006dd229a6bbf70a2d084a648
StevenPapalia <papaliasteven@gmail.com>
2020-02-20 12:29:57 -0800

    pseudocoded store, videoPlayerAction, wrapping of index.html in provided

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,5 +1,9 @@
 var changeVideo = (video) => ({
   //TODO:  Return some action object to change the currently playing video.
+  // return {
+  //  type: 'VIDEO_CHANGE',
+  //  currentVideo: video
+  // };
 });
 
 export default changeVideo;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,27 @@ import searchYouTube from './lib/searchYouTube.js';
 import YOUTUBE_API_KEY from './config/youtube.js';
 
 //TODO: Import the Provider component from 'react-redux' here!
+// import { Provider } from 'react-redux';
+// import configureStore from './store/configureStore';
 
 //TODO: Use the Provider component to make your store available to
 //  the rest of your app.
+// const store = configureStore(); //optional to pass in an initial stater here
 
 ReactDOM.render(
   <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />,
   document.getElementById('app')
 );
+
+
+
+
+
+// set up our store and wrap our app (<App />) to pass the store down as props:
+
+// ReactDOM.render(
+//   <provider store={store}>
+//     <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />
+//   </provider>,
+//   document.getElementById('app')
+// );
